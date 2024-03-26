8543be1523ddc839c6d13152a9daf60eaad50047
Łukasz <103317892+luksi02@users.noreply.github.com>
2024-03-26 22:48:21 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ const app = express();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+//app.use((req, res, next) => {
+  //res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Methods', 'POST');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //next();
+//});
 
 app.use(express.json({ limit: '50mb' }));
 
